3859147711c588bca130bab76aa27d221bb07e01
EDREYYY <nilo.edrei@escolar.ifrn.edu.br>
2025-05-26 11:44:03 -0300

    jogo butao

---
+++ motrores/Assets/movimento.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+    
+    private int numero;
+
+    public float velocidade = 20;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var numero = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(numero);
+        //numero = numero + 1; 
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+        }
+    }
+}
